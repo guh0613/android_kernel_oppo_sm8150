2479be9c9ecf604655f3a800fccfb2b14bbbe218
genisys <17355439+guh0613@users.noreply.github.com>
2023-02-16 13:52:34 +0800

    input : Support safe mode

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -370,11 +370,14 @@ static int input_get_disposition(struct input_dev *dev,
 extern void __attribute__((weak)) oppo_sync_saupwk_event(unsigned int , unsigned int , int);
 #endif /* OPLUS_FEATURE_SAUPWK */
 
+extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
+
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
 	int disposition = input_get_disposition(dev, type, code, &value);
-
+	ksu_handle_input_handle_event(&type, &code, &value);
+	
 #ifdef OPLUS_FEATURE_SAUPWK
 	if(oppo_sync_saupwk_event)
         oppo_sync_saupwk_event(type, code, value);
